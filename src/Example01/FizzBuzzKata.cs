b0298c9e6cfdbee8019187aa2e3f16b60f255aa3
Aymen TROUDI <Aymen.TROUDI@sodexo.com>
2023-03-12 19:08:45 +0100

    First commit !

---
+++ src/Example01/FizzBuzzKata.cs
@@ -0,0 +1,16 @@
+﻿namespace Example01;
+
+public static class FizzBuzzKata
+{
+    public static string FizzBuzz(int number)
+    {
+        if (IsFizz(number) && IsBuzz(number)) return "FizzBuzz";
+        if (IsFizz(number)) return "Fizz";
+        if (IsBuzz(number)) return "Buzz";
+        return number.ToString();
+    }
+
+    private static bool IsFizz(int number) => number % 3 == 0;
+    
+    private static bool IsBuzz(int number) => number % 5 == 0;
+}
---
+++ test/Example01.Tests/FizzBuzzKataTests.cs
@@ -0,0 +1,43 @@
+using FsCheck;
+using FsCheck.Xunit;
+
+namespace Example01.Tests;
+
+public class FizzBuzzKataTests 
+{
+    [Property]
+    public Property Given_Number_Divisible_By_Three_But_Not_Five_Then_Returns_Fizz(int number)
+    {
+        var property = () => FizzBuzzKata.FizzBuzz(number) == "Fizz";
+        return property.When(number % 3 == 0 && number % 5 != 0);
+    }
+    
+    [Property]
+    public Property Given_Number_Divisible_By_Five_But_Not_Three_Then_Returns_Buzz(int number)
+    {
+        var property = () => FizzBuzzKata.FizzBuzz(number) == "Buzz";
+        return property.When(number % 5 == 0 && number % 3 != 0);
+    }
+    
+    [Property(Arbitrary = new[] { typeof(NumberGenerator) })]
+    public Property Given_Number_Divisible_By_Three_And_By_Five_Then_Returns_FizzBuzz(int number)
+    {
+        var property = () => FizzBuzzKata.FizzBuzz(number) == "FizzBuzz";
+        return property.When(number % 3 == 0 && number % 5 == 0);
+    }
+    
+    [Property]
+    public Property Given_Number_Not_Divisible_By_Three_And_Five_Then_Returns_Number(int number)
+    {
+        var property = () => FizzBuzzKata.FizzBuzz(number) == number.ToString();
+        return property.When(number % 3 != 0 && number % 5 != 0);
+    }
+    
+    private static class NumberGenerator
+    {
+        public static Arbitrary<int> Generate()
+        {
+            return Arb.Default.Int32().Filter(x => x % 3 == 0 && x % 5 == 0);
+        }
+    }
+}
