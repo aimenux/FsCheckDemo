4a93b987b9e3e321458064265080787edc09406a
Aymen TROUDI <Aymen.TROUDI@sodexo.com>
2023-03-15 10:00:21 +0100

    Keep it simple

--- test/Example01.Tests/FizzBuzzKataTests.cs
+++ test/Example01.Tests/FizzBuzzKataTests.cs
@@ -10,29 +10,25 @@ public class FizzBuzzKataTests
     [Property(Arbitrary = new[] { typeof(DivisibleByThreeNotByFiveDataGenerator) }, MaxTest = MaximumNumber)]
     public Property Given_Number_Divisible_By_Three_But_Not_Five_Then_Returns_Fizz(int number)
     {
-        var property = () => FizzBuzzKata.FizzBuzz(number) == "Fizz";
-        return property.ToProperty();
+        return (FizzBuzzKata.FizzBuzz(number) == "Fizz").ToProperty();
     }
     
     [Property(Arbitrary = new[] { typeof(DivisibleByFiveNotByThreeDataGenerator) }, MaxTest = MaximumNumber)]
     public Property Given_Number_Divisible_By_Five_But_Not_Three_Then_Returns_Buzz(int number)
     {
-        var property = () => FizzBuzzKata.FizzBuzz(number) == "Buzz";
-        return property.ToProperty();
+        return (FizzBuzzKata.FizzBuzz(number) == "Buzz").ToProperty();
     }
     
     [Property(Arbitrary = new[] { typeof(DivisibleByThreeAndFiveDataGenerator) }, MaxTest = MaximumNumber)]
     public Property Given_Number_Divisible_By_Three_And_Five_Then_Returns_FizzBuzz(int number)
     {
-        var property = () => FizzBuzzKata.FizzBuzz(number) == "FizzBuzz";
-        return property.ToProperty();
+        return (FizzBuzzKata.FizzBuzz(number) == "FizzBuzz").ToProperty();
     }
     
     [Property(Arbitrary = new[] { typeof(NotDivisibleByThreeAndFiveDataGenerator) }, MaxTest = MaximumNumber)]
     public Property Given_Number_Not_Divisible_By_Three_And_Five_Then_Returns_Number(int number)
     {
-        var property = () => FizzBuzzKata.FizzBuzz(number) == number.ToString();
-        return property.ToProperty();
+        return (FizzBuzzKata.FizzBuzz(number) == number.ToString()).ToProperty();
     }
 
     private static class DivisibleByThreeNotByFiveDataGenerator
